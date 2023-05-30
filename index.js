726a5d9b525ee7b1173579f8435f1d535e166ebd
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2023-05-29 23:36:35 -0500

    feat: display remotes for each repositories

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import * as fs from "fs";
 import path from "path";
 import { fileURLToPath } from 'url';
 
-import { filterGitRepositories, walkFolders } from "./utils/index.js";
+import { filterGitRepositories, walkFolders, getGitStatus, getGitRemote } from "./utils/index.js";
+import { get } from "http";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -23,7 +24,22 @@ async function main(args) {
     console.log("Git Directories:");
     console.log("------------\n\n");
     const gitRepositories = filterGitRepositories(folderPaths);
-    console.log(gitRepositories);
+    // console.log(gitRepositories);
+    const paths = gitRepositories.map((gitRepository) => gitRepository.folderPath)
+    console.log(paths.length + " git repositories found\n\n");
+    for(const gitRepository of gitRepositories) {
+        // const status = await getGitStatus(path);
+        // console.log(status);
+        try {
+            const remote = await getGitRemote(gitRepository.folderPath);
+            console.log(remote.length > 0 ? remote : "No remote found for " + gitRepository.folderPath);
+            console.log("\n\n");
+        } catch (error) {
+            console.log(error.message ?? "No remote found for " + path);
+            console.log("\n\n");
+        }
+       
+    }
     console.log("------------\n\n");
     console.log('args', args);
 }
