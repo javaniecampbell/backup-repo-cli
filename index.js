b3a5a5aa18fc31ca5f25487241310af139ace57c
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2023-05-30 20:05:17 -0500

    feat: stubbed the steps  to be  executed

--- index.js
+++ index.js
@@ -8,6 +8,34 @@ import { get } from "http";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 async function main(args) {
+    // await testRun(args);
+
+    // STEP 1:      traverse/walk all folders and subfolders recursively
+    
+    // STEP 2:      filter out all git repositories by checking if .git folder exists
+    // STEP 3:      for each git repository, get the git remote
+    // STEP 4:      check if there is a remote with the name "origin" or any other name
+    // STEP 5:      if exists check the git status of the repository
+    // STEP 6:      if there are any changes, then add all the changes by running the command "git add -A && git commit -m 'commit message'"
+    // STEP 6.1:    if there are unpushed changes, then push all the changes by running the command "git push --all --follow-tags"
+    // STEP 7:      if there is no remote with the name "origin" or any other name, then add the remote by running the command "git remote add origin <url>"
+    // STEP 7.1:    if there is no remote then create one by running the command "gh repo create <repo-name> --source=. --public/--private --confirm --push"
+ 
+}
+
+const args = process.argv.slice(2);
+
+(async () => {
+    await main(args);
+})();
+
+main(args).then(() => {
+    console.log("\n\nDone 🚀🚀");
+}
+).catch((err) => {
+    console.log(err);
+});
+async function testRun(args) {
     const folderPaths = [path.resolve(__dirname, process.cwd())];
     // const folderPath = path.resolve(__dirname, "./")
     // const results = fs.readdirSync(path.resolve(__dirname, "./"));
@@ -25,9 +53,9 @@ async function main(args) {
     console.log("------------\n\n");
     const gitRepositories = filterGitRepositories(folderPaths);
     // console.log(gitRepositories);
-    const paths = gitRepositories.map((gitRepository) => gitRepository.folderPath)
+    const paths = gitRepositories.map((gitRepository) => gitRepository.folderPath);
     console.log(paths.length + " git repositories found\n\n");
-    for(const gitRepository of gitRepositories) {
+    for (const gitRepository of gitRepositories) {
         // const status = await getGitStatus(path);
         // console.log(status);
         try {
@@ -38,21 +66,9 @@ async function main(args) {
             console.log(error.message ?? "No remote found for " + path);
             console.log("\n\n");
         }
-       
+
     }
     console.log("------------\n\n");
     console.log('args', args);
 }
 
-const args = process.argv.slice(2);
-
-// (async () => {
-//     await main(args);
-//  })();
-
-main(args).then(() => {
-    console.log("\n\nDone 🚀🚀");
-}
-).catch((err) => {
-    console.log(err);
-});
